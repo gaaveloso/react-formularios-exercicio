c1b67352a14403bcd6801e8c459f96fa8cdfe29a
gaaveloso <gabrielvelosorezende-po@hotmail.com>
2022-12-08 18:04:27 -0300

    exercicio resolvido

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,31 +1,51 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm'
 import { MainContainer, Form, Input } from './styles'
 
 function MainPage() {
- 
+
+  const [formulario, onChange] = useForm({ nome: "", modulos: "", tecnologias: "", responsavel: ""})
+
+  const handleClick = (event) => {
+    console.log(formulario);
+    event.preventDefault()
+  }
+
   return (
     <MainContainer>
       <h2>Cadastro de novo curso</h2>
 
-      <Form onSubmit="">
-        <label for="nome">Nome do Curso:</label>
+      <Form onSubmit={handleClick}>
+        <label htmlFor="nome">Nome do Curso:</label>
         <Input 
           id="nome"
+          name="nome"
+          value={formulario.nome}
+          onChange={onChange}
         />
 
-        <label for="modulos">Número de Módulos: </label>
+        <label htmlFor="modulos">Número de Módulos: </label>
         <Input 
           id="modulos"
+          name="modulos"
+          value={formulario.modulos}
+          onChange={onChange}
         />
 
-        <label for="tecnologias">Tecnologias abordadas: </label>
+        <label htmlFor="tecnologias">Tecnologias abordadas: </label>
         <Input 
           id="tecnologias"
+          name="tecnologias"
+          value={formulario.tecnologias}
+          onChange={onChange}
         />
         
-        <label for="responsavel">Responsável: </label>
+        <label htmlFor="responsavel">Responsável: </label>
         <Input 
           id="responsavel"
+          name="responsavel"
+          value={formulario.responsavel}
+          onChange={onChange}
         />
       <button type="submit">Criar curso</button>
       </Form>
